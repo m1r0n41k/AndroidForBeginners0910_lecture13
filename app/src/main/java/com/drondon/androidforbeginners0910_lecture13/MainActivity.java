bf29f7e4d7e4ef79f359e1e4670bec1673a1756e
Andrii Miroshnychenko <collosteam@gmail.com>
2017-11-20 22:03:23 +0200

    Save Instance State

--- app/src/main/java/com/drondon/androidforbeginners0910_lecture13/MainActivity.java
+++ app/src/main/java/com/drondon/androidforbeginners0910_lecture13/MainActivity.java
@@ -12,63 +12,35 @@ import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView counterTextView;
-    Button buttonStart;
-    private static final String TAG = "MainActivity";
+    private static final String STATE_LEVEL = "key.level";
+    private int currentLevel = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        counterTextView = findViewById(R.id.counter);
-        buttonStart = findViewById(R.id.start);
-
-        buttonStart.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                MyDownloadTask task = new MyDownloadTask();
-                task.execute("http://mysite.com/logo.png");
-            }
-        });
+        // Сохранненные данные можно восстановить сдесь
+        if (savedInstanceState != null) {
+            currentLevel = savedInstanceState.getInt(STATE_LEVEL);
+        }
+        currentLevel++;
+        Log.d(TAG, "onCreate: " + currentLevel);
     }
 
-    class MyDownloadTask extends AsyncTask<String, Integer, File> {
+    private static final String TAG = "MainActivity_";
 
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            Log.d(TAG, "onPreExecute: " + Thread.currentThread().getName());
-            counterTextView.setText("Подготовка...");
-        }
+    // Сохраняем данные
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putInt(STATE_LEVEL, currentLevel);// ++ для примера
+        super.onSaveInstanceState(savedInstanceState);
+    }
 
-        @Override
-        protected File doInBackground(String... strings) {
-            Log.d(TAG, "doInBackground: " + Thread.currentThread().getName());
-            for (int i = 0; i < 100; i++) {
-                try {
-                    Thread.sleep(500);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                publishProgress(i);
-            }
-
-            return new File("/my_file.png");
-        }
-
-        @Override
-        protected void onProgressUpdate(Integer... values) {
-            super.onProgressUpdate(values);
-            Log.d(TAG, "onProgressUpdate: " + Thread.currentThread().getName());
-            counterTextView.setText("Загружено " + (values[0] + 1) + " %");
-        }
-
-        @Override
-        protected void onPostExecute(File file) {
-            super.onPostExecute(file);
-            Log.d(TAG, "onPostExecute: " + Thread.currentThread().getName());
-            counterTextView.setText("Готово!");
-        }
+    // Восстанавливаем сохраненные  данные
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        //currentLevel = savedInstanceState.getInt(STATE_LEVEL);
     }
 
 }
